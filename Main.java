b70683c90bdceb6bf2a9fa1be7de0d1cd4b3027c
shushma990 <72930652+shushma990@users.noreply.github.com>
2020-11-13 16:33:35 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main{
+public static void main(String []args)
+{
+boolean flag=true;
+System.out.println("flag");
+byte range=124;
+System.out.println("range");
+short temperature=-200;
+System.out.println("temperature");
+int range=-4250000;
+System.out.println("range");
+long range=-43545454444L;
+System.out.println("range");
+double number=-42.7;
+System.out.println("number");
+float number=-42.3f;
+System.out.println("number");
+char letter='\u0051';
+System.out.println("letter");
+}}
